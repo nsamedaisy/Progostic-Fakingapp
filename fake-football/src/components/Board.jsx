eaaf4e8d25ae9739d64e3853babdb816a7e37f84
nsamedaisy <nsamedaisy@gmail.com>
2023-06-12 10:26:13 +0100

    my first commit

---
+++ fake-football/src/components/Board.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card, Content } from 'antd'
+
+function Board() {
+  return (
+    <Card style={{ margin: 70, backgroundColor: '#05722', color: 'white' }}>Board</Card>
+  )
+}
+
+export default Board
